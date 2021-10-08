a59abfbcc03316bca05f6f6c5ed4a85ab296c37b
Parthvi-patel <78634250+Parthvi-patel@users.noreply.github.com>
2021-10-08 16:55:16 +0530

    Add files via upload

---
+++ submission.java
@@ -0,0 +1,15 @@
+package src.main;
+
+import java.util.Scanner;
+
+public class submission {
+    public static void main(String[] args) {
+        Scanner s1= new Scanner(System.in);
+        System.out.println("Enter value of A and B");
+        int A=s1.nextInt();
+        int B=s1.nextInt();
+
+        int c=A+B;
+        System.out.println("Submission="+c);
+    }
+}
